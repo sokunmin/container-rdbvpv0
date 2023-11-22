6e6199d796fa4d03a3054cc1865c12bde9ca165f
Chun-Ming <sokunmin@gmail.com>
2023-11-22 23:51:29 +0800

    feat: add Poe installation using modal-client

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,10 @@
 set -ex
 
 # Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+#exec gunicorn -w 4 -b '0.0.0.0:5003' --access-logfile=- 'app:app'
+
+
+#git clone https://github.com/poe-platform/server-bot-quick-start
+cd server-bot-quick-start
+pip install -r requirements.txt
+modal deploy main.py
